e963ff8721707b547872628ce51d94ae6e6c167b
Daria Vasilega <dariavasilega@gmail.com>
2023-08-31 17:34:53 +0300

    Resolves #7: Test CRUD.
    - Added CRUD for `test` entity.

--- app/routes.php
+++ app/routes.php
@@ -13,4 +13,15 @@ return function (\Slim\App $app) {
             $router->delete('', \App\Application\Actions\Lection\Delete::class);
         });
     });
+
+    // Test CRUD
+    $app->group('/test', function (\Slim\Interfaces\RouteCollectorProxyInterface $router) {
+        $router->post('', \App\Application\Actions\Test\Create::class);
+        $router->get('s', \App\Application\Actions\Test\ReadList::class);
+        $router->group('/{id:[0-9]+}', function (\Slim\Interfaces\RouteCollectorProxyInterface $router) {
+            $router->get('', \App\Application\Actions\Test\Read::class);
+            $router->put('', \App\Application\Actions\Test\Update::class);
+            $router->delete('', \App\Application\Actions\Test\Delete::class);
+        });
+    });
 };
